ec4eb19b5252400ca25e5681845f01d082bd5950
Luiz Quirino <luizfpq@gmail.com>
2020-10-08 00:44:02 -0300

    change packages; add cronometroController

---
+++ app/Controllers/Http/CronometroController.js
@@ -0,0 +1,29 @@
+'use strict'
+
+class CronometroController {
+
+    start({view}) {
+    setInterval(function(){
+    
+        let novaHora = new Date();
+        // getHours trará a hora
+        // geMinutes trará os minutos
+        // getSeconds trará os segundos
+        let hora = novaHora.getHours();
+        let minuto = novaHora.getMinutes();
+        let segundo = novaHora.getSeconds();
+    
+        // Chamamos a função zero para que ela retorne a concatenação
+        // com os minutos e segundos
+        minuto = zero(minuto);
+        segundo = zero(segundo);
+    
+        // Com o textContent, iremos inserir as horas, minutos e segundos
+        // no nosso elemento HTML
+        document.getElementById('hora').textContent = hora+':'+minuto+':'+segundo;
+    },1000)
+}
+
+}
+
+module.exports = CronometroController
